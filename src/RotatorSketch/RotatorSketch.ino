2719e5676f0c697661a32d213f5b3d8b36f07a4c
bjohan <sa6bxq>
2017-01-13 17:56:20 +0100

    work in progress

--- src/RotatorSketch/RotatorSketch.ino
+++ src/RotatorSketch/RotatorSketch.ino
@@ -1,5 +1,6 @@
 #include <EEPROM.h>
 #include <Servo.h>
+#include <Wire.h>
 
 extern "C"
 {
@@ -35,8 +36,25 @@ Servo rotserv;
 void setup()
 {
   Serial.begin(9600);
+	rotserv.attach(9);
 }
 
+
+uint8_t checkAddress(uint8_t addr){
+	Wire.beginTransmission(addr);
+	return Wire.endTransmission() == 0;
+}
+
+uint8_t readRegister(uint8_t devaddr, uint8_t regaddr){
+	uint8_t data;
+	Wire.beginTransmission(devaddr);
+	Wire.write(regaddr);
+	
+	Wire.requestFrom((int)devaddr, (int)1);
+	data = Wire.read();
+	Wire.endTransmission();
+	return data;
+}
 
 void printValue(char *name, float value)
 {
@@ -49,7 +67,20 @@ void printValue(char *name, float value)
 
 void loop()
 { 
-	rotserv.attach(9);
+	//LSM303D 0x1D Accelerometer and magnetometer
+	//BMP180 0x77 Pressure sensor
+	//L3GD20 0x6B Gyro
+	//0x1D, 0x6B 0x77
+	/*if(checkAddress(0x1D))
+		Serial.println("Device present");
+	else
+		Serial.println("no device");
+		
+        for(int i = 0 ; i <255 ; i++){
+		printValue("addr", i);
+		printValue("Acc", readRegister(0x77, i));
+		Serial.println("");
+	}*/
 	commandLine();
 }
 
